14c77bbc575dd2df410ba6c7d8e96998afb22513
yosri-hk <yosri.hajkhlifa@esprit.tn>
2023-10-26 22:07:27 +0100

    controller

--- src/main/java/com/example/store/controller/StoreRestController.java
+++ src/main/java/com/example/store/controller/StoreRestController.java
@@ -1,4 +1,43 @@
 package com.example.store.controller;
 
+import com.example.store.entity.Store;
+import com.example.store.repos.StoreRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@RestController
+@RequestMapping("/store")
 public class StoreRestController {
+    @Autowired
+    StoreRepository storeRepo;
+
+
+    @PostMapping("/addStore")
+    public Store addStore(@RequestBody Store store) {
+        return storeRepo.save(store);
+    }
+
+    @GetMapping("")
+    public List<Store> ListStores () {
+        return storeRepo.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Store storeById (@PathVariable Integer id) {
+        return storeRepo.findById(id).orElse(null);
+    }
+
+
+    @PutMapping("/updatestore")
+    public Store updateStore(@RequestBody Store store) {
+        return storeRepo.save(store);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteStore (@PathVariable Integer id) {
+        if (storeRepo.findById(id).isPresent())
+            storeRepo.deleteById(id);
+    }
+
 }
