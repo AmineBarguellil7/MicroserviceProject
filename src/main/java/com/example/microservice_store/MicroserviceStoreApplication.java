3fc00618020c5d9bb67c0b5ea07f56652e0d4876
DhifallahAhmed <dhifallahahmed92@gmail.com>
2023-10-26 15:00:38 +0100

    entity store

---
+++ src/main/java/com/example/microservice_store/MicroserviceStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.microservice_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceStoreApplication.class, args);
+    }
+
+}
---
+++ src/main/java/entity/Store.java
@@ -0,0 +1,28 @@
+package entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Store {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String location;
+    private String phoneNumber;
+    private String productCategory;
+
+}
